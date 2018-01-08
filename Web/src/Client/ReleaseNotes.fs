706da48cd2090f1b8e6475f4c89bca788dc6dcde
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-01-08 20:11:35 +0000

    Bumping version to 0.0.2

--- Web/src/Client/ReleaseNotes.fs
+++ Web/src/Client/ReleaseNotes.fs
@@ -1,6 +1,6 @@
 module internal ReleaseNotes
 
-let Version = "0.0.1"
+let Version = "0.0.2"
 
 let IsPrerelease = false
 
@@ -8,6 +8,6 @@ let Notes = """
 ### 0.0.1 - 2017-10-04
 * Initial version
 
-### 0.0.1a - 2018-01-08
+### 0.0.2 - 2018-01-08
 * Change port in Dockerfile
 """
