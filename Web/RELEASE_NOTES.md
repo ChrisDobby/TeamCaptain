6a1b31dcebedda074397e1dd4f242e9a4c350562
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-01-08 20:09:44 +0000

    Bumping version to 0.0.1

--- Web/RELEASE_NOTES.md
+++ Web/RELEASE_NOTES.md
@@ -1,2 +1,5 @@
 ### 0.0.1 - 2017-10-04
 * Initial version
+
+### 0.0.1a - 2018-01-08
+* Change port in Dockerfile
--- Web/src/Client/ReleaseNotes.fs
+++ Web/src/Client/ReleaseNotes.fs
@@ -7,4 +7,7 @@ let IsPrerelease = false
 let Notes = """
 ### 0.0.1 - 2017-10-04
 * Initial version
+
+### 0.0.1a - 2018-01-08
+* Change port in Dockerfile
 """
