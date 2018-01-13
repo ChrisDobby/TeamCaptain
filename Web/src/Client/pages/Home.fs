9ee4c832f889de023ff1752f1bb19a5659a5dc63
Chris Dobson <dobby@chrisjdobson.co.uk>
2018-01-13 20:25:37 +0000

    Changed home page

--- Web/src/Client/pages/Home.fs
+++ Web/src/Client/pages/Home.fs
@@ -6,16 +6,22 @@ open Fable.Helpers.React.Props
 open Messages
 open Client.Style
 
-let view (dispatch: AppMsg -> unit) = 
+let view (dispatch: AppMsg -> unit) =
     [
         div [ ClassName "container" ]
             [
-            div [ ClassName "card-panel blue darken-3 z-depth-5"]
+            div [ ClassName "card blue-text text-darken-3 z-depth-2"]
                 [
-                    p [ ClassName "white-text"] [
-                        h3 [] [str "To register with a team, create a new team or update your details"]
-                      ]
-                    buttonLink "btn" (fun _ -> dispatch ShowLogin) [str "Login"]
+                    div [ClassName "card-content"]
+                        [
+                        p [] [
+                            h3 [] [str "To register with a team, create a new team or update your details"]
+                          ]
+                        ]
+                    div [ClassName "card-action"]
+                        [
+                            buttonLink "" (fun _ -> dispatch ShowLogin) [str "Login"]
+                        ]
                 ]
             ]
     ]
